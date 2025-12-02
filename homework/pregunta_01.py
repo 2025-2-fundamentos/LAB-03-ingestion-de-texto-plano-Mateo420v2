8295da3a3eaf0c2df53ea112b481a419a539627f
Mateo López Campo <matlopezca@unal.edu.co>
2025-12-02 17:57:53 -0500

    lab 3
    a

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
@@ -18,3 +20,50 @@ def pregunta_01():
 
 
     """
+
+    with open("files/input/clusters_report.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+    
+    columns = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]
+    
+
+    data = []
+    current_row = None  
+    cluster_counter = 1 
+    
+
+    for line in lines[4:]:
+        if line.strip():  
+            if line[:4].strip().isdigit():
+                if current_row:  
+                    data.append(current_row)
+                cluster = cluster_counter
+                cluster_counter += 1  
+                cantidad = int(line[5:18].strip())
+                porcentaje = float(line[19:33].strip().replace(",", ".").replace("%", ""))
+                palabras_clave = line[34:].strip()
+                current_row = [cluster, cantidad, porcentaje, palabras_clave]
+            else:
+               
+                if current_row:
+                    current_row[-1] += " " + line.strip()
+
+
+    if current_row:
+        data.append(current_row)
+    df = pd.DataFrame(data, columns=columns)
+    df["principales_palabras_clave"] = (
+        df["principales_palabras_clave"]
+        .str.replace("\s+", " ", regex=True)
+        .str.replace(", ", ",")
+        .str.replace(",", ", ")
+        .str.strip()
+        .str.rstrip(".")
+    )
+    
+    return df
+
+if __name__ == "__main__":
+    resultado = pregunta_01()
+    output_file = "output.txt"
+    resultado.to_csv(output_file, sep="\t", index=False)
